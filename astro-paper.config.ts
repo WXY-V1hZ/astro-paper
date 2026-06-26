93c62105a0c991ae075d6ccd7df56e4ff296258b
WXY-V1hZ <nenood1091@gmail.com>
2026-06-26 21:33:10 +0800

    1

---
+++ astro-paper.config.ts
@@ -0,0 +1,46 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://blog.wxiny.com",
+    title: "唯一赫兹",
+    description: "V1hZ's Blog",
+    author: "V1hZ",
+    profile: undefined,
+    ogImage: "default-og.jpg",
+    lang: "zh-CN",
+    timezone: "Asia/Shanghai",
+    dir: "auto",
+    // googleVerification: undefined
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: true,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: false,
+      // url: "https://github.com/satnaing/astro-paper/edit/main/",
+    },
+    search: "pagefind",
+  },
+  socials: [
+    // { name: "github",   url: "https://github.com/satnaing/astro-paper" },
+    // { name: "x",        url: "https://x.com/username" },
+    // { name: "linkedin", url: "https://www.linkedin.com/in/username/" },
+    // { name: "mail",     url: "mailto:yourmail@gmail.com" },
+  ],
+  shareLinks: [
+    // { name: "whatsapp", url: "https://wa.me/?text=" },
+    // { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    // { name: "x",        url: "https://x.com/intent/post?url=" },
+    // { name: "telegram", url: "https://t.me/share/url?url=" },
+    // { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
+    // { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
